949812d5e3fc6382f1fd1b8535784ad6f41d0ea4
John Zielke <j.l.zielke@gmail.com>
2018-03-21 15:14:31 +0100

    Fix Port issue

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use('/', express.static('presentation'), serveIndex('public', {'icons': true
 app.use('/hello',function(req,res){
   res.end('Hello ' + (req.query['name'] || 'World'))
 })
-const server = app.listen(3000, function(){
+const port = process.env['PORT'] || 3000
+const server = app.listen(port, function(){
   console.log('server is running at %s', server.address().port);
 });
